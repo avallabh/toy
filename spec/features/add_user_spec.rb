bd0b68fb0ad29adcb23faa1e71b3edeb767068bc
Ajay Vallabh <ajay.vallabh@mail.com>
2014-01-02 22:07:29 -0500

    Added failed JS for onCLick Markers

---
+++ spec/features/add_user_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature 'add map', %Q{
+As a user
+I want to see the map
+So I can find/add a restroom
+} do
+
+# ACCEPTANCE CRITERIA
+# * Display a map
+# * Let user add a pin
+
+  scenario 'display map' do
+    visit restrooms_path
+
+    expect(page).to have_content('Index')
+  end
+
+end
