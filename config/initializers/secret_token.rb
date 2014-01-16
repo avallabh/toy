eaa53cd49d4402a6687ee51cacf4518587941210
Ajay Vallabh <ajay.vallabh@mail.com>
2014-01-16 11:05:59 -0500

    Changed secret_key_base to ENV var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Toy::Application.config.secret_key_base = '7c2ff28289ddc557a404bf50fbec800130dd5aed550b82590fb9a9d46b4dcc1cb07f6304391761491310369f909dc40616f1fc6a16d187dc89d68322698150f5'
+Toy::Application.config.secret_key_base = ENV['SECRET_TOKEN']
